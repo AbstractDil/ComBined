5ed066fcf8f69054554e14a95098847bf03e4e37
AbstractDil <nandysagar@yahoo.com>
2022-09-24 22:02:39 +0530

    updated

---
+++ components/Notification.js
@@ -0,0 +1,120 @@
+
+
+import Link from  'next/link';
+
+
+
+
+const  LatestNews = () => {
+
+  
+
+  return (
+    <div>
+        
+  <div className="container  mt-3  " >
+
+    <div className="row">
+    
+                 {/* CGLE 2022   start */}
+
+                    <div className=" col-md-12" >
+
+<div className="  mb-3 justify-content-center">
+
+<div className="col-md-12 mb-3 alert alert-info border-1 " >
+<p className="font-weight-bold text-center">
+
+
+                  
+                  <span className="label label-danger blink">New</span>
+                  <span className="titleOne ">SSC CGLE 2022 (Sept.)</span><span className="label label-danger blink ">New</span>
+                 
+                  <br />
+                 <br />
+                  <span className="text-danger"><span className='glyphicon glyphicon-hand-right'></span> Application Start  :</span>  17/09/2022 10 AM  <br/>
+                <span className='text-danger'><span className='glyphicon glyphicon-hand-right'></span> Application End : </span>   08/10/2022 (up to 11:00 p.m)<br/>
+                <span className='text-danger'><span className='glyphicon glyphicon-hand-right'></span>  Edit Window :  </span> 12.10.2022 to 13.10.2022 (upto 11:00 p.m)<br/>
+                <span className='text-danger'><span className='glyphicon glyphicon-hand-right'></span>  Application Fees : </span>  Rs. 100/- ( No fee for SC/ST/PH/Ex-Servicemen/Women candidates)<br/>
+
+           
+                   
+                   
+                  
+
+
+                </p>
+                <hr className='hr-info' />
+                <div className="text-center">
+                  <Link href=" https://ssc.nic.in/CandidatePortal/CandidatePortal" >
+                <a type='button' className='btn btn-warning'><i className='fa fa-pencil-square-o'></i> Apply Now</a>
+                </Link>
+                <Link href="https://ssc.nic.in/SSCFileServer/PortalManagement/UploadedFiles/notice_CGLE_17092022.pdf">
+                    <a type='button' className='btn btn-down'><i className='fa fa-download'></i> Notification </a>
+                    </Link> 
+                </div>
+                
+            </div>
+            
+        </div>
+
+  
+  </div>
+              
+            {/* cgle 2022 ends    */}
+
+            
+                 {/* FCI Category 3 2022   start */}
+
+                 <div className=" col-md-12" >
+
+<div className="  mb-3 justify-content-center">
+
+<div className="col-md-12 mb-3 alert alert-info border-1 " >
+<p className="font-weight-bold text-center">
+
+
+                  
+                  <span className="label label-danger blink">New</span>
+                  <span className="titleOne ">FCI Category III ZONE-WISE RECRUITMENT OF NON-EXECUTIVES IN FCI</span><span className="label label-danger blink ">New</span>
+                 
+                  <br />
+                 <br />
+                  <span className="text-danger"><span className='glyphicon glyphicon-hand-right'></span> Application Start  :</span>  06/09/2022 10:00 AM" <br/>
+                <span className='text-danger'><span className='glyphicon glyphicon-hand-right'></span> Application End : </span>   05/10/2022 04:00 PM <br/>
+                <span className='text-danger'><span className='glyphicon glyphicon-hand-right'></span>  Edit Window :  </span>05/10/2022 04:00 PM<br/>
+                <span className='text-danger'><span className='glyphicon glyphicon-hand-right'></span>  Application Fees : </span> Rs. 500/- for General, OBC and EWS candidates and Rs. Nil for SC/ST/PWD/Women candidates <br/>
+
+           
+                   
+                   
+                  
+
+
+                </p>
+                <hr className='hr-info' />
+                <div className="text-center">
+                  <Link href=" https://ibpsonline.ibps.in/fcineaug22/index.php" >
+                <a type='button' className='btn btn-warning'><i className='fa fa-pencil-square-o'></i> Apply Now</a>
+                </Link>
+                <Link href="https://www.careerpower.in/2022/fci-ag3-notification.pdf">
+                    <a type='button' className='btn btn-down'><i className='fa fa-download'></i> Notification </a>
+                    </Link> 
+                </div>
+                
+            </div>
+            
+        </div>
+
+  
+  </div>
+              
+            {/* FCI Category 3 2022 ends    */}
+  
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default LatestNews
